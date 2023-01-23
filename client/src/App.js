5e9a257aa86cf092b4977d8d6926f196816e2729
Laura Ness <lness@ucla.edu>
2023-01-22 22:34:32 -0800

    pls deploy pls

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from "axios";
+import spider from "./spiderman.jpg";
+import "./styles.css";
+
+export default class App extends React.Component {
+  state = {
+    users: [],
+  };
+  componentDidMount() {
+    const headers = {
+      'Authorization': 'Bearer my-token',
+      'My-Custom-Header': 'flag{sp1d3rm4n_d03s_wh4t3v3r_a_sp1d3r_c4n}'
+    };
+    axios.get("/users.json", {headers: {'My-Custom-Header': 'flag{sp1d3rm4n_d03s_wh4t3v3r_a_sp1d3r_c4n}'}}).then((response) => {
+      this.setState({ users: response.data });
+    });
+  }
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div>
+        <h1>your friendly neighborhood villain registry!</h1>
+        <img src = {spider} alt = "spider"></img>
+        <ul className="users">
+          {users.map((user) => (
+            <li className="user">
+              <p>
+                <strong>Name:</strong> {user.name}
+              </p>
+              <p>
+              <strong>Alias:</strong> {user.username}
+              </p>
+            </li>
+          ))}
+        </ul>
+
+        <p></p>
+      </div>
+    );
+  }
+}
